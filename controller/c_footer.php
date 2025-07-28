d9ad6fb66e643c5cc19c28e4e41c07afa918d7ed
Iria :D <iriabherrero@gmail.com>
2025-07-28 20:18:33 +0200

    Arreglar footer

--- controller/c_footer.php
+++ controller/c_footer.php
@@ -1,16 +1,17 @@
 <?php 
-    session_start();
     require_once __DIR__.'/../model/connectaDb.php';
 
     require_once __DIR__.'/../model/productes.php';
 
-    $productes = [];
+    $products = [];
     
-    // Per cada producte del cistell, agafem la seva informació del carret
-    foreach($_SESSION["producte"] as $productId) {
+    // Por cada producto de la cesta, añadimos su información al carrito
+    if (!empty($_SESSION["producte"])) {
+        foreach($_SESSION["producte"] as $productId) {
         $product = getProductesById($productId[0]);
-        $productes[] = [$product[0], $productId[1]];
+        $products[] = [$product[0], $productId[1]];
+        }
     }
-
+    
     require_once __DIR__.'/../view/v_footer.php';
 ?>
