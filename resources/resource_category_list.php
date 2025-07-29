b3df7aef006e2b6b32af7d56eb587bff750534a0
Iria :D <iriabherrero@gmail.com>
2025-07-29 15:29:40 +0200

    Arreglar apartado categorias

--- resources/resource_category_list.php
+++ resources/resource_category_list.php
@@ -3,20 +3,20 @@
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
         <title> Categories </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="/resources/css/llistatProductes.css" />
-        <link rel="stylesheet" type="text/css" href="/resources/css/producte.css" />
-        <link rel="stylesheet" type="text/css" href="/resources/css/categories.css" title="main"/>
+        <link rel="stylesheet" type="text/css" href="resources/css/llistatProductes.css" />
+        <link rel="stylesheet" type="text/css" href="resources/css/producte.css" />
+        <link rel="stylesheet" type="text/css" href="resources/css/categories.css" title="main"/>
         
     </head>
 
     <body>
-        <?php require '/home/TDIW/tdiw-l1/public_html/controller/c_header.php'; ?>
+        <?php require __DIR__.'/../controller/c_header.php'; ?>
 
         <div class="main-content-eliminar detalls">
-            <?php require '/home/TDIW/tdiw-l1/public_html/controller/c_llistatCategories.php'; ?> 
+            <?php require __DIR__.'/../controller/c_llistatCategories.php'; ?> 
         </div>
 
-        <?php require '/home/TDIW/tdiw-l1/public_html/controller/c_footer.php'; ?>
+        <?php require __DIR__.'/../controller/c_footer.php'; ?>
 
     </body>
 </html>
