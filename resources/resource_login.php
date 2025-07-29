a5017b1bfe2f7b61956f9887cdd541bbc4097c14
Iria :D <iriabherrero@gmail.com>
2025-07-29 15:22:54 +0200

    Arreglar css login

--- resources/resource_login.php
+++ resources/resource_login.php
@@ -2,7 +2,7 @@
     <head>
         <title> Login </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="/resources/css/estilFormularis.css" />
+        <link rel="stylesheet" type="text/css" href="resources/css/estilFormularis.css" />
     </head>
 
     <body>
