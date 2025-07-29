9049b44737a8677e51bee5b98a65fce5231be225
Iria :D <iriabherrero@gmail.com>
2025-07-29 15:33:19 +0200

    Arreglar jQuery para generar contenido dinámico

--- view/jQueryProvaDetallProducte.js
+++ view/jQueryProvaDetallProducte.js
@@ -4,9 +4,9 @@ $(document).on("click", ".button-productes", function(event) {
     event.preventDefault(); 
     var category = $(this).find('p').attr('id'); // Trobem l'atribut corresponent a la id de la categoria
     var categoryId = parseInt(category, 10);
-
+    
     // Carreguem la nova part de la pàgina
-    $('.main-content-eliminar').load('/controller/c_llistatProductes.php?category_id=' + categoryId);
+    $('.main-content-eliminar').load('controller/c_llistatProductes.php?category_id=' + categoryId);
 });
 
 // Carregar els detalls sobre el producte
@@ -16,7 +16,7 @@ $(document).on("click", ".button-producte", function(event) {
     var producte = $(this).find('p').attr('id'); // Trobem l'atribut corresponent a la id del producte
     var producteId = parseInt(producte, 10);
     // Carreguem la nova part de la pàgina
-    $('.productes').load('/controller/c_detallsProducte.php?producteId=' + producteId);
+    $('.productes').load('controller/c_detallsProducte.php?producteId=' + producteId);
 });
 
 // Afegir productes al carret
@@ -27,18 +27,18 @@ $(document).on("click", ".afegir-producte", function(event) {
     var producteId = $(this).attr('value'); 
 
     // Carreguem la nova part de la pàgina
-    $('.detalls').load('/controller/c_afegirProducte.php?producteId=' + producteId + '&quantitat=' + quantitat, function() {
-        $('header').load('/controller/c_header.php');
-        $('.detalls').load('/controller/c_detallsProducte.php?producteId=' + producteId);
-        $('footer').load('/controller/c_footer.php');
+    $('.detalls').load('controller/c_afegirProducte.php?producteId=' + producteId + '&quantitat=' + quantitat, function() {
+        $('header').load('controller/c_header.php');
+        $('.detalls').load('controller/c_detallsProducte.php?producteId=' + producteId);
+        $('footer').load('controller/c_footer.php');
     });
 });
 
-// Carregar resultats de la busqueda
+// Carregar resultats de la busqueda    
 $(document).on("click", ".boton-search", function(event) {
     // Evitem que fagi l'acció del enllaç
     event.preventDefault();
     var searchText = $('input[name="text-search"]').val();
     // Carreguem la nova part de la pàgina
-    $('.body').load('/controller/c_resultats.php?search=' + encodeURIComponent(searchText));
+    $('.body').load('controller/c_resultats.php?search=' + encodeURIComponent(searchText));
 }); 
